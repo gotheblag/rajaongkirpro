3636691e8dd0af0d21c893a2c6c6984995ba7334
erfanbagussetiana <erfanbagus12345@gmail.com>
2018-04-03 21:27:52 +0700

    update dikirim dari

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -2,6 +2,7 @@
 
 namespace erfanbagussetiana\rajaongkirpro;
 
+use erfanbagussetiana\rajaongkirpro\app\Dikirim_dari;
 use erfanbagussetiana\rajaongkirpro\app\International;
 use erfanbagussetiana\rajaongkirpro\app\Kecamatan;
 use erfanbagussetiana\rajaongkirpro\app\Provinsi;
@@ -24,7 +25,9 @@ class RajaOngkir {
     public function international(){
 	    return new International;
     }
-
+    public function dikirim_dari(){
+        return new Dikirim_dari;
+    }
 	public function Cost($attributes){
 		return new Cost($attributes);
 	}
